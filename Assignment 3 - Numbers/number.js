cebfef5fe2c4d25aa9de5c0f1d32a3f538f35ad2
Magnus <79435486+mhvec@users.noreply.github.com>
2021-03-15 21:58:41 -0400

    Add files via upload

---
+++ Assignment 3 - Numbers/number.js
@@ -0,0 +1,44 @@
+let img = new Image();
+img.src = 'numbers2.png';
+img.onload = function() {
+  init();
+};
+const scale = 1;
+const width = 120;
+const height = 175;
+const scaledWidth = scale * width;
+const scaledHeight = scale * height;
+
+function drawFrame(frameX, frameY, canvasX, canvasY) {
+  ctx.drawImage(img,
+                frameX * width, frameY * height, width, height,
+                canvasX, canvasY, scaledWidth, scaledHeight);
+}
+
+const cycleLoop = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let currentLoopIndex = 0;
+let frameCount = 0;
+
+function step() {
+  frameCount++;
+  if (frameCount < 120) {
+    window.requestAnimationFrame(step);
+    return;
+  }
+  frameCount = 0;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0);
+  currentLoopIndex++;
+  if (currentLoopIndex >= cycleLoop.length + 1) {
+  drawFrame(1, 0, 0, 0);
+  drawFrame(0, 0, width, 0);
+  setTimeout(() => {  ctx.clearRect(0, 0, canvas.width, canvas.height); }, 500);  
+  currentLoopIndex = 0;
+  }
+  window.requestAnimationFrame(step);
+}
+
+function init() {
+  window.requestAnimationFrame(step);
+}
+
